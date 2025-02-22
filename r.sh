04f80185b0f231f9cb63689e9f856d862983f82a
Hady <hady_awayda@hotmail.com>
2025-02-22 04:42:05 +0200

    Updated test cases

--- r.sh
+++ r.sh
@@ -1,6 +1,8 @@
 make te
 
-./minishell_test 'echo $'
+./minishell_test 'echo a b'
 # valgrind --leak-check=full --show-leak-kinds=definite --track-origins=yes --suppressions=utils/valgrind.supp ./minishell_test 'echo $USER"a"'
 
 make tclean
+
+# echo a  b |
