b85b8be103bc302db8a53651023c0bf3cf471757
Nathan <nataneabbas@gmail.com>
2025-04-30 12:22:27 +0300

    fixed some norminette erros in builtins , but still not final version !

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: nabbas <nabbas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 09:28:01 by nabbas            #+#    #+#             */
-/*   Updated: 2025/04/23 14:12:23 by nabbas           ###   ########.fr       */
+/*   Updated: 2025/04/30 12:18:48 by nabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,87 +16,71 @@
 #include <limits.h>
 
 /*
-** convert_to_ll:
-**   Parse an optional single '+' or '-' then digits.
-**   Return 1 on success (store in *out), 0 on any error (nondigit or overflow).
+** convert_to_ll: parse optional +/- then digits with overflow check
 */
-static int convert_to_ll(const char *s, long long *out)
+static int	convert_to_ll(const char *s, long long *out)
 {
-    int          sign = 1;
-    long long    num  = 0;
-    int          i    = 0;
-    int          d;
+	int		 i;
+	int		 sign;
+	unsigned long long	 num;
+	int		 d;
 
-    if ((s[i] == '+' || s[i] == '-') &&
-        !(s[i+1] == '+' || s[i+1] == '-'))
-        sign = (s[i++] == '-') ? -1 : 1;
-
-    if (!s[i]) /* no digits */
-        return (0);
-
-    while (s[i])
-    {
-        if (!ft_isdigit(s[i]))
-            return (0);
-        d = s[i++] - '0';
-        if (sign == 1)
-        {
-            if (num > (LLONG_MAX - d) / 10)
-                return (0);
-        }
-        else
-        {
-            if ((unsigned long long)num >
-                (((unsigned long long)LLONG_MAX + 1ULL) - (unsigned long long)d) / 10)
-                return (0);
-        }
-        num = num * 10 + d;
-    }
-    *out = sign * num;
-    return (1);
+	i = 0;
+	sign = 1;
+	if ((s[i] == '+' || s[i] == '-')
+		&& s[i + 1] != '\0')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+	if (s[i] == '\0')
+		return (0);
+	num = 0;
+	while (s[i])
+	{
+		if (!ft_isdigit(s[i]))
+			return (0);
+		d = s[i++] - '0';
+		if (sign == 1
+			&& num > (LLONG_MAX - d) / 10)
+			return (0);
+		if (sign == -1
+			&& num > ((unsigned long long)LLONG_MAX + 1ULL - d) / 10)
+			return (0);
+		num = num * 10 + d;
+	}
+	*out = sign * num;
+	return (1);
 }
 
 /*
-** process_exit:
-**   – Always prints "exit\n"
-**   – No args: exit(0)
-**   – One arg:
-**       • non-numeric → error, return 1
-**       • numeric     → exit(code % 256)
-**   – >1 arg:
-**       • first non-numeric → error, return 1
-**       • first numeric     → "too many arguments", return 1
+** process_exit: print exit and handle numeric argument
 */
-int process_exit(char **args, char **envp)
+int	process_exit(char **args, char **envp)
 {
-    int          count = 0;
-    long long    val;
+	int			count;
+	long long	val;
 
-    (void)envp;
-    ft_putendl_fd("exit", 1);
-    while (args[count])
-        count++;
-
-    /* case: no argument */
-    if (count == 1)
-        exit(0);
-
-    /* try parsing first argument */
-    if (!convert_to_ll(args[1], &val))
-    {
-        write(2, "bash: exit: ", 12);
-        write(2, args[1],   ft_strlen(args[1]));
-        write(2, ": numeric argument required\n", 29);
-        return (1);
-    }
-
-    /* too many args? */
-    if (count > 2)
-    {
-        write(2, "bash: exit: too many arguments\n", 31);
-        return (1);
-    }
-
-    /* one valid number */
-    exit((unsigned char)val);
+	(void)envp;
+	ft_putendl_fd("exit", 1);
+	count = 0;
+	while (args[count])
+		count++;
+	if (count == 1)
+		exit(0);
+	if (!convert_to_ll(args[1], &val))
+	{
+		write(2, "bash: exit: ", 12);
+		write(2, args[1], ft_strlen(args[1]));
+		write(2, ": numeric argument required\n", 29);
+		return (1);
+	}
+	if (count > 2)
+	{
+		write(2, "bash: exit: too many arguments\n", 31);
+		return (1);
+	}
+	exit((unsigned char)val);
+	return (0);
 }
